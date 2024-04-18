9c15941e471964c143f2204f016567088894b293
Ashley Michell L <ashley.michell02@gmail.com>
2024-04-18 12:45:57 -0400

    primer commit

---
+++ laesquina/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/ashley/proyectosbootcam/unidad_5/desafio_entornovirtual/laesquina/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
